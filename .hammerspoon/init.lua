ff97973c1eea63ae60c03a83de1eb173186fb3cf
Hao Li <hli@carnegielearning.com>
2020-06-11 15:49:59 -0400

    Add hello world notification

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,3 +4,7 @@ require('layout')
 
 local rescueWindows = require "rescuewindows"
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", rescueWindows)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
