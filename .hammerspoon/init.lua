b0cb1344c4de2926066c3f4f4f54e481341ab071
Hao Li <haoli2@andrew.cmu.edu>
2020-06-12 23:10:38 -0400

    Add wifi watcher

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,8 @@
 require('config')
 -- require('rescuewindow')
 require('layout')
+require('finder_watcher')
+require('wifi_watcher')
 
 local rescueWindows = require "rescuewindows"
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", rescueWindows)
@@ -8,3 +10,8 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", rescueWindows)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
